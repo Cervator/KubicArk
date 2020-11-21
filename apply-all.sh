47ec8253d98a77b5fabdc3672b38c478a7ac1ccd
Cervator <cervator@gmail.com>
2020-11-20 23:15:57 -0600

    Maybe sleeping a bit would be helpful

--- apply-all.sh
+++ apply-all.sh
@@ -3,5 +3,6 @@ kubectl apply -f ark-pvc-shared.yaml
 kubectl apply -f ArkManagerCfgCM.yaml
 kubectl apply -f GlobalGameUserSettingsCM.yaml
 kubectl apply -f OverrideGameUserSettingsCM.yaml
-kubectl apply -f ark-deployment.yaml
 kubectl apply -f ark-service.yaml
+sleep 10
+kubectl apply -f ark-deployment.yaml
