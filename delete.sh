a5e4c81b4f900b4e2d750fdb2473dd01681bcbd3
Cervator <cervator@gmail.com>
2020-11-26 15:41:12 -0600

    Delete deployment before volume

--- delete.sh
+++ delete.sh
@@ -2,10 +2,10 @@
 if [ $# -eq 1 ]
 then
   echo "Going to delete stuff for the ARK server running $1";
+  kubectl delete -f $1/ark-deployment.yaml
   kubectl delete -f $1/ark-pvc.yaml
   kubectl delete -f $1/OverrideGameUserSettingsCM.yaml
   kubectl delete -f $1/OverrideGameIniCM.yaml
-  kubectl delete -f $1/ark-deployment.yaml
   # TODO: Consider using a stateful set just to get a cleaner pod name? Only ever 0 or 1 instances ...
 else
   echo "Didn't get exactly one arg, so will delete global things instead. Got $# ! $*"
