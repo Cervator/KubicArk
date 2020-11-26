95c3839576b330ef265b2e555949d1106abb08c5
Cervator <cervator@gmail.com>
2020-11-26 11:15:23 -0600

    The big split - two servers, two dirs, and the mystery of whether ports will work ...

---
+++ apply-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -eq 1 ]
+then
+  echo "Going to work with the ARK server running $1";
+  kubectl apply -f $1/ark-pvc.yaml
+  kubectl apply -f ark-pvc-shared.yaml
+  kubectl apply -f ArkManagerCfgCM.yaml
+  kubectl apply -f GlobalGameUserSettingsCM.yaml
+  kubectl apply -f $1/OverrideGameUserSettingsCM.yaml
+  kubectl apply -f GlobalGameIniCM.yaml
+  kubectl apply -f $1/OverrideGameIniCM.yaml
+  kubectl apply -f ArkPlayerListsCM.yaml
+  echo "Hope you remembered to update the passwords in the secrets file only locally!"
+  kubectl apply -f ark-server-secrets.yaml
+  kubectl apply -f ark-service.yaml
+  kubectl apply -f $1/ark-deployment.yaml
+  # TODO: Consider using a stateful set just to get a cleaner pod name? Only ever 0 or 1 instances ...
+else
+  echo "Didn't get exactly one arg, got $# ! $*"
+  echo "Valid server/map names are: isle, cent, scorc, rag, ab, ext, valg, gen1, cryst"
+fi
---
+++ delete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 1 ]
+then
+  echo "Going to delete stuff for the ARK server running $1";
+  kubectl delete -f $1/ark-pvc.yaml
+  kubectl delete -f $1/OverrideGameUserSettingsCM.yaml
+  kubectl delete -f $1/OverrideGameIniCM.yaml
+  kubectl delete -f $1/ark-deployment.yaml
+  # TODO: Consider using a stateful set just to get a cleaner pod name? Only ever 0 or 1 instances ...
+else
+  echo "Didn't get exactly one arg, so will delete global things instead. Got $# ! $*"
+  echo "Valid server/map names are: isle, cent, scorc, rag, ab, ext, valg, gen1, cryst"
+  kubectl apply -f ark-pvc-shared.yaml
+  kubectl apply -f ArkManagerCfgCM.yaml
+  kubectl apply -f GlobalGameUserSettingsCM.yaml
+  kubectl apply -f GlobalGameIniCM.yaml
+  kubectl apply -f ArkPlayerListsCM.yaml
+  kubectl apply -f ark-server-secrets.yaml
+  kubectl apply -f ark-service.yaml
+fi
