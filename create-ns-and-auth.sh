2d91b817b6337525aba040526975f688845cbc4b
Rasmus Praestholm <rpraestholm@adaptavist.com>
2021-05-31 22:18:29 -0500

    Add some useful logging

--- create-ns-and-auth.sh
+++ create-ns-and-auth.sh
@@ -3,3 +3,4 @@ export targetns=ark
 echo "Going to create the namespace $targetns and the auth pieces for a service user"
 kubectl create namespace $targetns
 kubectl apply -f auth
+kubectl describe sa ark-user -n ark
