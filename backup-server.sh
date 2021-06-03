5b9b8918ea908398a47adefcb073853089abc1ec
Rasmus Praestholm <rpraestholm@adaptavist.com>
2021-06-03 01:08:55 -0500

    Fine-tune a bit

--- backup-server.sh
+++ backup-server.sh
@@ -3,14 +3,18 @@ export targetns=ark
 if [ $# -eq 1 ]
 then
   echo "Going to back up the ARK map $1 giving it 30 seconds to get far enough for a file to be ready";
-  #kubectl delete -f $1/backup-job.yaml -n ark
+  kubectl get jobs -n ark
+  kubectl delete -f $1/backup-job.yaml -n ark --ignore-not-found=true
+  sleep 5
+  echo "Deleted old job, if present"
   kubectl get jobs -n ark
   kubectl apply -f $1/backup-job.yaml -n ark
   kubectl get jobs -n ark
   sleep 30
+  echo "Slept 30 secs, job should not be finished yet, so we can do the copy before it exits"
   kubectl get jobs -n ark
   pod=$(kubectl get pods -n ark --selector=job-name=ark${1}backup --output=jsonpath='{.items[*].metadata.name}')
-  echo $pod
+  echo "Job pod to target is: $pod"
   kubectl cp ark/$pod:/ark/server/ShooterGame/Saved/SavedArks/${1}_backup.tar.gz ${1}_backup.tar.gz
   ls -la
 else
