02d7acb0149e896b716943b5d97fb4204b864020
Rasmus Praestholm <rpraestholm@adaptavist.com>
2021-06-03 00:26:55 -0500

    Adjust more naming

--- backup-server.sh
+++ backup-server.sh
@@ -2,16 +2,16 @@
 export targetns=ark
 if [ $# -eq 1 ]
 then
-  echo "Going to back up the ARK map gen1 then wait 30 seconds to see if it has finished";
-  #kubectl delete -f gen1/backup-job.yaml -n ark
+  echo "Going to back up the ARK map $1 then wait 30 seconds to see if it has finished";
+  #kubectl delete -f $1/backup-job.yaml -n ark
   kubectl get jobs -n ark
-  kubectl apply -f gen1/backup-job.yaml -n ark
+  kubectl apply -f $1/backup-job.yaml -n ark
   kubectl get jobs -n ark
   sleep 30
   kubectl get jobs -n ark
-  pod=$(kubectl get pods -n ark --selector=job-name=arkgenesisbackup --output=jsonpath='{.items[*].metadata.name}')
+  pod=$(kubectl get pods -n ark --selector=job-name=arkgen1backup --output=jsonpath='{.items[*].metadata.name}')
   echo $pod
-  kubectl cp ark/$pod:/ark/server/ShooterGame/Saved/SavedArks/genesis_backup.tar.gz genesis_backup.tar.gz
+  kubectl cp ark/$pod:/ark/server/ShooterGame/Saved/SavedArks/gen1_backup.tar.gz gen1_backup.tar.gz
   ls -la
 else
   echo "Didn't get exactly one arg, so won't try to back anything up: $*"
