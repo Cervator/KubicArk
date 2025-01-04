d403678a01e36bc8846eb94bbf9579d7bdf348c5
Cervator <cervator@gmail.com>
2025-01-04 00:13:46 -0500

    Now with 100% fewer arrays

--- backup-server.sh
+++ backup-server.sh
@@ -6,27 +6,54 @@
 # gcloud upload that file to a bucket
 
 #!/bin/bash
+server_name=$1
 
-# Map of server names to their .ark file names
-server_maps=("island=island.ark", "center=center.ark", "scorched=scorched.ark", "ragnarok=ragnarok.ark", "aberration=aberration.ark", "extinction=extinction.ark", "valguero=Valguero_P.ark", "genesis1=Genesis.ark", "genesis2=Gen2.ark", "crystal=crystal.ark", "lost=LostIsland.ark", "fjordur=Fjordur.ark")
+# Function to get the .ark file name for a server (avoids using an array in case of unsupportive shells)
+get_ark_file() {
+  local server_name=$1
+
+  if [ "$server_name" = "island" ]; then
+    echo "island.ark"
+  elif [ "$server_name" = "center" ]; then
+    echo "center.ark"
+  elif [ "$server_name" = "scorched" ]; then
+    echo "scorched.ark"
+  elif [ "$server_name" = "ragnarok" ]; then
+    echo "ragnarok.ark"
+  elif [ "$server_name" = "aberration" ]; then
+    echo "aberration.ark"
+  elif [ "$server_name" = "extinction" ]; then
+    echo "extinction.ark"
+  elif [ "$server_name" = "valguero" ]; then
+    echo "Valguero_P.ark"
+  elif [ "$server_name" = "genesis1" ]; then
+    echo "Genesis.ark"
+  elif [ "$server_name" = "genesis2" ]; then
+    echo "Gen2.ark"
+  elif [ "$server_name" = "crystal" ]; then
+    echo "crystal.ark"
+  elif [ "$server_name" = "lost" ]; then
+    echo "LostIsland.ark"
+  elif [ "$server_name" = "fjordur" ]; then
+    echo "Fjordur.ark"
+  else
+    echo "Unknown server: $server_name"
+    return 1 # Indicate an error
+  fi
+}
 
 if [ $# -eq 1 ]
 then
+  ark_file=$(get_ark_file "$server_name")
 
-  # Get the server name from the command-line argument
-  server_name=$1
+  # Check if the function returned an error
+  if [ $? -ne 0 ]; then
+    echo "Error getting ark file name."
+    exit 1
+  fi
 
-  # Find the element in the array that matches the server name
-  for entry in "${server_maps[@]}"; do
-    key=$(echo "$entry" | cut -d '=' -f 1)
-    if [ "$key" = "$server_name" ]; then
-      ark_file=$(echo "$entry" | cut -d '=' -f 2)
-      break
-    fi
-  done
-
-  # Print the .ark file name for debugging
   echo "$server_name uses the ark file name: $ark_file"
+
   kubectl get ns
 
   # Execute the backup command
